8e04b5e26db3057a8b6699947666bc1611b78a96
Kalindu-Karunarathna <kalindukarunarathne626@gmail.com>
2025-12-12 19:56:29 +0530

    Initial commit: added project files

---
+++ main.py
@@ -0,0 +1,17 @@
+import pandas
+import turtle
+
+
+screen = turtle.Screen()
+screen.title("US States Game")
+image = "blank_states_img.gif"
+
+screen.addshape(image)
+turtle.shape(image)
+
+
+
+screen.exitonclick()
+
+
+
